ebeece4a463047b0faeb75934c12c594316439ef
trunghn2003 <trung@labs.com.vn>
2025-04-01 09:19:12 +0700

    change port

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:9000',
+  baseURL: import.meta.env.VITE_API_URL || 'http://127.0.0.1:9000',
 });
 
 // Request interceptor - thêm token vào header
