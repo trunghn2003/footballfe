d8e09a7a6148585b24962395c4f771b49e5e9cd3
trunghn2003 <trung@labs.com.vn>
2025-04-01 09:29:15 +0700

    update api host

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://127.0.0.1:9000',
+  baseURL: import.meta.env.VITE_API_URL || 'http://157.66.47.51:9000',
 });
 
 // Request interceptor - thêm token vào header
